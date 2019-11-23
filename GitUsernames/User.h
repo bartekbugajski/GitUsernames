1df6b1cdd590efe7cafb97e70ca7f1d00bffab6f
Bartek Bugajski <bartekbugajski@hotmail.com>
2019-11-23 15:34:00 +0100

    first version, struggling with the API

---
+++ GitUsernames/User.h
@@ -0,0 +1,19 @@
+//
+//  User.h
+//  GitUsernames
+//
+//  Created by Bartek Bugajski on 23/11/2019.
+//  Copyright © 2019 BB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property (strong, nonatomic) NSString *name;
+//@property (strong, nonatomic) NSString *avatar;
+//@property (strong, nonatomic) NSString *id;
+
+
+@end
+
--- GitUsernames/ViewController.m
+++ GitUsernames/ViewController.m
@@ -7,17 +7,93 @@
 //
 
 #import "ViewController.h"
+#import "User.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate,UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (weak, nonatomic) NSMutableArray<User *> *usernames;
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+ //   [self setupUsernames];
+    [self fetchUsersUsingJSON];
+    
+    self.navigationItem.title = @"Git Usernames";
+    self.navigationController.navigationBar.prefersLargeTitles = NO;
+//
+//    self.tableView.delegate = self;
+//    self.tableView.dataSource = self;
+//
+    
 }
+
+
+- (void)fetchUsersUsingJSON {
+    NSLog(@"Fetching usernames");
+    NSString *urlString = @"https://api.letsbuildthatapp.com/jsondecodable/courses";
+    NSURL *url = [NSURL URLWithString:urlString];
+    [[NSURLSession.sharedSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"Usernames fetching finished.");
+        
+        
+        NSError *err;
+        NSArray *usersJSON = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&err];
+        if (err){
+            NSLog(@"Failed to serialize into JSON: %@", err);
+            return;
+        }
+        
+        NSMutableArray<User *> *usernames = NSMutableArray.new;
+        for (NSDictionary *usersDictionary in usersJSON) {
+            NSString *name = usersDictionary[@"name"];
+           // NSString *avatar = usersDictionary[@"avatar_url"];
+            //NSString *id = usersDictionary[@"id"];
+            User *username = User.new;
+            username.name = name;
+            //username.avatar = avatar;
+            //username.id = id;
+            [usernames addObject:username];
+        
+        }
+        NSLog(@"%@", usernames);
+     self.usernames = usernames;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
+
+    }] resume];
+}
+
+//pramga mark - UITableView DataSource Methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {return self.usernames.count;}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellId = @"cell";
+    
+    //UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellId];
+    UITableViewCell *cell = [[UITableViewCell alloc]
+                             initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
+    
+//    UITableViewCell *cell = [[tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
+    
+    User *username = self.usernames[indexPath.row];
+    
+    cell.textLabel.text = username.name;
+    //cell.detailTextLabel.text = username.avatar;
+    //cell.detailTextLabel.text = username.id;
+
+    return cell;
+}
+
 
 
 @end
